023fa854471fab5bee71c1872f8691f127159ab0
Muhammet <33325253+blackdragonx61@users.noreply.github.com>
2021-01-09 18:01:02 +0300

    Update input_main.cpp

--- 1.Svn/Server/game/src/input_main.cpp
+++ 1.Svn/Server/game/src/input_main.cpp
@@ -2,24 +2,18 @@
 	if (!ch->IsEmptyItemGrid(p->ItemPos, pkItem->GetSize()))
 		return;
 		
-///Change
+///Add Above
 #if defined(SAFEBOX_CHECKOUT_UPDATE)
 	if (p->SelectPosAuto)
 	{
-		if (pkItem->IsDragonSoul())
-			p->ItemPos = TItemPos(DRAGON_SOUL_INVENTORY, ch->GetEmptyDragonSoulInventory(pkItem));
-		else
-			p->ItemPos = TItemPos(INVENTORY, ch->GetEmptyInventory(pkItem->GetSize()));
-	}
-	if (!ch->IsEmptyItemGrid(p->ItemPos, pkItem->GetSize()))
-	{
-		if (p->SelectPosAuto)
+		int AutoPos = pkItem->IsDragonSoul() ? ch->GetEmptyDragonSoulInventory(pkItem) : ch->GetEmptyInventory(pkItem->GetSize());
+		if (AutoPos == -1)
+		{
 			ch->ChatPacket(CHAT_TYPE_INFO, "You don't have enough space.");
-		return;
+			return;
+		}
+		p->ItemPos = TItemPos(pkItem->IsDragonSoul() ? DRAGON_SOUL_INVENTORY : INVENTORY, AutoPos);
 	}
-#else
-	if (!ch->IsEmptyItemGrid(p->ItemPos, pkItem->GetSize()))
-		return;
 #endif
 
 //Find in void CInputMain::SafeboxCheckin(LPCHARACTER ch, const char * c_pData)
